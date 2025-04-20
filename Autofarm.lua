7419b59871693244846ea98aee5d15815ed007ad
Sol1-xyz <109892692+Sol1-xyz@users.noreply.github.com>
2025-04-20 20:44:50 +0200

    Update Autofarm.lua

--- Autofarm.lua
+++ Autofarm.lua
@@ -1,1 +1,25 @@
 
+repeat wait() until game:IsLoaded()
+ if game.PlaceId == 112315720097464 then
+ 
+ local ohTable1 = {
+ 	["Location"] = "Orc Lands",
+ 	["GroupType"] = "Private",
+ 	["Difficult"] = "Easy",
+ 	["Invasions"] = false
+ }
+ local ohTable2 = {
+ 	["ReduceMaxHPBy70"] = false,
+ 	["NoPotions"] = true,
+ 	["ReduceDamageBy50"] = false,
+ 	["Damage2x"] = false
+ }
+ queue_on_teleport('repeat wait() until game:IsLoaded(); loadstring(game:HttpGet("https://pastebin.com/raw/bLNHj0WB"))()')
+ game:GetService("ReplicatedStorage").Events.CreateDungeonGroup:FireServer(ohTable1, ohTable2)
+ wait(0.5)
+ game:GetService("ReplicatedStorage").Events.StartDungeonGroup:FireServer()
+ end
+ 
+
+
+

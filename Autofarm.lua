44acd63ec5e3d659c74550abc242faf6f6248bbe
Sol1-xyz <109892692+Sol1-xyz@users.noreply.github.com>
2025-04-21 02:18:46 +0200

    Update Autofarm.lua

--- Autofarm.lua
+++ Autofarm.lua
@@ -1,32 +1,76 @@
+for _, conn in pairs(getconnections(game:GetService("RunService").RenderStepped)) do
+    conn:Disconnect()
+end
 
-repeat wait() until game:IsLoaded()
+-- Check if we’re in lobby
+local ohTable1 = {
+	["Location"] = "Orc Lands",
+	["GroupType"] = "Public",
+	["Difficult"] = "Easy",
+	["Invasions"] = false
+}
+local ohTable2 = {
+	["NoPotions"] = true
+}
+
+game:GetService("ReplicatedStorage").Events.CreateDungeonGroup:FireServer(ohTable1, ohTable2)
+
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
 repeat wait() until player.Character or player.CharacterAdded:Wait()
 local character = player.Character
 
-repeat wait() until character:FindFirstChild("Humanoid") and character:FindFirstChild("ShortSwords")
- if game.PlaceId == 112315720097464 then
- wait(2)
- local ohTable1 = {
- 	["Location"] = "Orc Lands",
- 	["GroupType"] = "Private",
- 	["Difficult"] = "Easy",
- 	["Invasions"] = false
- }
- local ohTable2 = {
- 	["ReduceMaxHPBy70"] = false,
- 	["NoPotions"] = true,
- 	["ReduceDamageBy50"] = false,
- 	["Damage2x"] = false
- }
- queue_on_teleport('repeat wait() until game:IsLoaded(); loadstring(game:HttpGet("https://pastebin.com/raw/bLNHj0WB"))()')
- game:GetService("ReplicatedStorage").Events.CreateDungeonGroup:FireServer(ohTable1, ohTable2)
- wait(0.5)
- game:GetService("ReplicatedStorage").Events.StartDungeonGroup:FireServer()
- end
- 
+repeat wait() until character:FindFirstChild("Humanoid") 
+
+repeat wait() until game:IsLoaded()
+
+local placeId = game.PlaceId
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 
 
+local UIInventoryController = require(game:GetService("Players").LocalPlayer.PlayerScripts.UIController.UIInventoryController)
+
+
+local sellableIds = {}
+
+for _, itemData in pairs(UIInventoryController.savedInventory) do
+    local itemId = nil
+    local rarity = nil
+
+    -- Loop through each field in the item
+    for key, value in pairs(itemData) do
+        if key == "Id" then
+            itemId = value
+        elseif key == "Rarity" then
+            rarity = value
+        end
+    end
+
+    -- If the rarity is NOT Epic or Legendary, save the ID
+    if rarity ~= "Epic" and rarity ~= "Legendary" and itemId then
+        table.insert(sellableIds, itemId)
+    end
+end
+
+game:GetService("ReplicatedStorage").Events.SellItems:InvokeServer(sellableIds)
+
+
+
+if placeId == 112315720097464 then
+
+
+
+
+queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/Sol1-xyz/Dungeon/refs/heads/main/Lobby.lua"))()')
+
+    wait(0.5)
+    ReplicatedStorage.Events.StartDungeonGroup:FireServer()
+  
+
+
+else
+    -- Farming logic (auto loaded via the queued teleport script)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Sol1-xyz/Dungeon/refs/heads/main/Lobby.lua"))()
+end

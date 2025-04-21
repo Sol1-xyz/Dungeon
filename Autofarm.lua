0db198bf93f0b78dd81123511422a176e05533fe
Sol1-xyz <109892692+Sol1-xyz@users.noreply.github.com>
2025-04-21 02:53:02 +0200

    Update Autofarm.lua

--- Autofarm.lua
+++ Autofarm.lua
@@ -71,7 +71,7 @@ if placeId == 112315720097464 then
 
 queue_on_teleport('loadstring(game:HttpGet("https://pastebin.com/raw/SGCXA4rM"))()')
 
-    wait(0.5)
+    wait(1)
     ReplicatedStorage.Events.StartDungeonGroup:FireServer()
   
 
